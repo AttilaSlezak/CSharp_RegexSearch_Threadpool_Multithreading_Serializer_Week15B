7e0979c5f481ce4ff866c5bfe34cd5507b2885b1
Slezak Attila <attila.slezak@gmail.com>
2016-05-03 15:23:52 +0100

    ThreadPoolDemo is ready

---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void ShowMyText(object state)
+        {
+            string myText = (string)state;
+            Console.WriteLine("Thread: {0} - {1}", Thread.CurrentThread.ManagedThreadId, myText);
+        }
+
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "God dag!");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Console.Read();
+        }
+    }
+}
