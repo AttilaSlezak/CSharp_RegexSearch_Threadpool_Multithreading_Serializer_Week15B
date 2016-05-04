b7d789bb6cd0af17323fd1b60bb66a5adf8e0435
Slezak Attila <attila.slezak@gmail.com>
2016-05-04 01:19:41 +0100

    Multithreading has been added

---
+++ Multithreading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    class Program
+    {
+        static void Counting()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Count: {0} - Thread: {1}", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = new ThreadStart(Counting);
+            Thread first = new Thread(threadStart);
+            Thread second = new Thread(threadStart);
+
+            first.Start();
+            second.Start();
+
+            first.Join();
+            second.Join();
+        }
+    }
+}
